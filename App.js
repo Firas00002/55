3ccf92f8023e4f439f706d63611522b75e4de0e5
Firas00002 <91484251+Firas00002@users.noreply.github.com>
2021-11-17 11:23:52 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Formbst from './componenets/Formbst';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Buttonbst from './componenets/Buttonbst';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Formbst/>
+        <Buttonbst/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
